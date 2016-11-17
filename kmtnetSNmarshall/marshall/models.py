592323485c6a94ff1f17182a1600bfcab0d48af7
Curtis McCully <cmccully@lcogt.net>
2016-11-17 09:32:09 -0800

    Updated the data model. Foreign keys return the full object, not just the id of one to many relationships.

--- kmtnetSNmarshall/marshall/models.py
+++ kmtnetSNmarshall/marshall/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import numpy as np
 
+
 class Field(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
@@ -20,8 +21,8 @@ class Candidate(models.Model):
     id = models.AutoField(primary_key=True)
     ra = models.FloatField()
     dec = models.FloatField()
-    field_id = models.ForeignKey(Field, on_delete=models.CASCADE)
-    quadrant_id = models.ForeignKey(Quadrant, on_delete=models.CASCADE)
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
+    quadrant = models.ForeignKey(Quadrant, on_delete=models.CASCADE)
     classification = models.ForeignKey(Classification, on_delete=models.CASCADE)
 
     def is_same_target(self, ra, dec):
@@ -29,4 +30,3 @@ class Candidate(models.Model):
         ra2_rad, dec2_rad = np.deg2rad([ra, dec])
         distance_rad = np.arccos(np.sin(dec1_rad) * np.sin(dec2_rad) + np.cos(dec1_rad) * np.cos(dec2_rad) * np.cos(ra2_rad - ra1_rad))
         return np.rad2deg(distance_rad) < (1.0 / 3600.0)
-
