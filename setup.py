16b396f98f9b98983bcfefda10bccef240027438
Maria Drout <mrdrout@gmail.com>
2016-12-14 16:37:19 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ksnex',
+      author=['Maria Drout', 'Curtis McCully'],
+      author_email=['maria.drout@dunlap.utoronto.ca', 'cmccully@lco.global'],
+      version=0.1,
+      packages=['ksnex'],
+      setup_requires=[],
+      install_requires=['numpy', 'astropy'],
+      tests_require=[]
+      )
