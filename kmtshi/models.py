1c788e964581b89a92ad08d14d33f27dc1fee0c3
Maria Drout <mrdrout@gmail.com>
2016-12-19 12:12:13 -0800

    Wiped migrations to try to debug

--- kmtshi/models.py
+++ kmtshi/models.py
@@ -27,13 +27,13 @@ class Classification(models.Model):
 @python_2_unicode_compatible #unicode support for Python 2
 class Candidate(models.Model):
     id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100,default="KSP-%s")
+    date_disc = models.FloatField(default=160000)
     ra = models.FloatField()
     dec = models.FloatField()
     field = models.ForeignKey(Field, on_delete=models.CASCADE)
     quadrant = models.ForeignKey(Quadrant, on_delete=models.CASCADE)
     classification = models.ForeignKey(Classification, on_delete=models.CASCADE)
-    date_disc = models.FloatField(default=160000)
-    name = models.CharField(max_length=100,default="KSP-%s")
     def __str__(self):
         return self.name
 
