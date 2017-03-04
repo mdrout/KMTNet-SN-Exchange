3c1761757a1bcfea8cff4776ded9406edf15b75a
Maria Drout <mrdrout@gmail.com>
2017-03-04 17:31:28 -0500

    Fix for photom model

--- kmtshi/models.py
+++ kmtshi/models.py
@@ -3,32 +3,39 @@ from django.utils.encoding import python_2_unicode_compatible
 from kmtshi.coordinates import great_circle_distance
 from django.utils import timezone
 
-@python_2_unicode_compatible #unicode support for Python 2
+
+@python_2_unicode_compatible  # unicode support for Python 2
 class Field(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
     subfield = models.CharField(max_length=20)
+
     def __str__(self):
         return self.subfield
 
-@python_2_unicode_compatible #unicode support for Python 2
+
+@python_2_unicode_compatible  # unicode support for Python 2
 class Quadrant(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
+
     def __str__(self):
         return self.name
 
-@python_2_unicode_compatible #unicode support for Python 2
+
+@python_2_unicode_compatible  # unicode support for Python 2
 class Classification(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
 
-@python_2_unicode_compatible #unicode support for Python 2
+
+@python_2_unicode_compatible  # unicode support for Python 2
 class Candidate(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100,default="KSP-%s")
+    name = models.CharField(max_length=100, default="KSP-%s")
     date_disc = models.DateTimeField()
     ra = models.FloatField()
     dec = models.FloatField()
@@ -38,6 +45,7 @@ class Candidate(models.Model):
     disc_im = models.ImageField(max_length=1000)
     disc_ref = models.ImageField(max_length=1000)
     disc_sub = models.ImageField(max_length=1000)
+
     def __str__(self):
         return self.name
 
@@ -54,7 +62,8 @@ class Candidate(models.Model):
         return great_circle_distance(self.ra, self.dec,
                                      candidate.ra, candidate.dec) < (1.0 / 3600.0)
 
-@python_2_unicode_compatible #unicode support for Python 2
+
+@python_2_unicode_compatible  # unicode support for Python 2
 class Comment(models.Model):
     id = models.AutoField(primary_key=True)
     candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
@@ -69,7 +78,8 @@ class Comment(models.Model):
     def __str__(self):
         return self.text
 
-@python_2_unicode_compatible #unicode support for Python 2
+
+@python_2_unicode_compatible  # unicode support for Python 2
 class PngImages(models.Model):
     id = models.AutoField(primary_key=True)
     candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
@@ -85,6 +95,7 @@ class PngImages(models.Model):
     def __str__(self):
         return self.candidate.name
 
+
 @python_2_unicode_compatible  # unicode support for Python 2
 class Photometry(models.Model):
     id = models.AutoField(primary_key=True)
@@ -94,4 +105,7 @@ class Photometry(models.Model):
     filter = models.CharField(max_length=10)
     mag = models.FloatField()
     dmag = models.FloatField()
-    flag = models.BooleanField() #True = detection; False = Obs, but not in files.
+    flag = models.BooleanField()  # True = detection; False = Obs, but not in files.
+
+    def __str__(self):
+        return self.candidate.name
